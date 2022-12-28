86c0529c132390011014251143bff9097ecbac85
Juan <jcsantamar@gmail.com>
2022-12-28 18:55:28 +0100

    block BANNER funcionando

--- app/public/wp-content/themes/eventhorizon/src/index.js
+++ app/public/wp-content/themes/eventhorizon/src/index.js
@@ -1,6 +1,17 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import "../css/style.scss"
 
-import TestComponent from "./TestComponent";
+// Our modules / classes
+import MobileMenu from "./modules/MobileMenu"
+import HeroSlider from "./modules/HeroSlider"
+import GoogleMap from "./modules/GoogleMap"
+import Search from "./modules/Search"
+import MyNotes from "./modules/MyNotes"
+import Like from "./modules/Like"
 
-ReactDOM.render(<TestComponent />, document.querySelector("#app"))
+// Instantiate a new object using our modules/classes
+const mobileMenu = new MobileMenu()
+const heroSlider = new HeroSlider()
+const googleMap = new GoogleMap()
+const search = new Search()
+const myNotes = new MyNotes()
+const like = new Like()

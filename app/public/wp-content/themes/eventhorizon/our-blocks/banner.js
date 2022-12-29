2de5ba59f05030570d04d597ed19715b942a5b3d
Juan <jcsantamar@gmail.com>
2022-12-29 11:35:18 +0100

    Blocks personalizados v2 (tras la sec28/lec175)

--- app/public/wp-content/themes/eventhorizon/our-blocks/banner.js
+++ app/public/wp-content/themes/eventhorizon/our-blocks/banner.js
@@ -1,3 +1,5 @@
+import {InnerBlocks} from "@wordpress/block-editor"
+
 wp.blocks.registerBlockType("ourblocktheme/banner", {
   title: "Banner",
   edit: EditComponent,
@@ -5,23 +7,37 @@ wp.blocks.registerBlockType("ourblocktheme/banner", {
 })
 
 function EditComponent() {
+  const useMeLater = (
+    <>
+      <h1 className="headline headline--large">Welcome!!!</h1>
+      <h2 className="headline headline--medium">We think you&rsquo;ll like it here.</h2>
+      <h3 className="headline headline--small">
+        Why don&rsquo;t you check out the <strong>major</strong> you&rsquo;re interested in?
+      </h3>
+      <a href="#" className="btn btn--large btn--blue">
+        Find Your Major
+      </a>
+    </>
+  )
+
   return (
     <div className="page-banner">
       <div className="page-banner__bg-image" style={{ backgroundImage: "url('/wp-content/themes/eventhorizon/images/library-hero.jpg')" }}></div>
       <div className="page-banner__content container t-center c-white">
-        <h1 className="headline headline--large">Welcome!</h1>
-        <h2 className="headline headline--medium">We think you&rsquo;ll like it here.</h2>
-        <h3 className="headline headline--small">
-          Why don&rsquo;t you check out the <strong>major</strong> you&rsquo;re interested in?
-        </h3>
-        <a href="#" className="btn btn--large btn--blue">
-          Find Your Major
-        </a>
+        <InnerBlocks allowedBlocks={["core/paragraph", "core/heading", "core/list"]}/>
       </div>
     </div>
   )
 }
 
 function SaveComponent() {
-  return <p>This is from our block.</p>
+  return (
+    <div className="page-banner">
+      <div className="page-banner__bg-image" style={{ backgroundImage: "url('/wp-content/themes/eventhorizon/images/library-hero.jpg')" }}></div>
+      <div className="page-banner__content container t-center c-white">
+        <InnerBlocks.Content />
+        <h6>Sitio creado por Sergio EseEne (www.developer.com)</h6>
+      </div>
+    </div>
+  )
 }

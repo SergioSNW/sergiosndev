f2decf77102c5365b0ede4a442169c3aadc4268f
Juan <jcsantamar@gmail.com>
2023-01-31 21:00:32 +0100

    Probado bucle de SetTimeout

--- app/public/wp-content/themes/eventhorizon/src/pr_Juan/masterskills.js
+++ app/public/wp-content/themes/eventhorizon/src/pr_Juan/masterskills.js
@@ -51,3 +51,36 @@ function viewTipo(array, tipo) {
   }, "<hr>>>> <strong>Apartado de " + tipo.toUpperCase() + "</strong> <<<<br>");
   return rc;
 }
+
+function prFetch(uri, label) {
+  fetch(uri)
+    .then((res) => res.json())
+    .then((res) => {
+      res.map((v) => {
+        if (v.acf.skill) {
+          v.acf.skill.forEach((element) => {
+            arrWork.push(element.name);
+          });
+        }
+      });
+      console.log("Recopilado: ", arrWork);
+      var sum = masterSkills(arrWork);
+      vista = viewTipo(sum, "cono");
+      vista += viewTipo(sum, "habi");
+      vista += viewTipo(sum, "idio");
+
+      document.getElementById(label).innerHTML =
+        arrWork + "<hr>" + JSON.stringify(sum) + vista;
+    })
+    .catch((err) => console.error(err));
+}
+
+function retardo(delay) {
+  let timerId = setTimeout(function plan() {
+    console.log("msg desde plan. Delay:", delay, ", id_timer: ", timerId);
+    delay = delay < 100 ? 0 : delay / 2;
+    if (delay > 0) {
+      timerId = setTimeout(plan, delay);
+    }
+  }, delay);
+}
